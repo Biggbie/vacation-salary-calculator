cc78466fd2eb74341ac6930fc167f3039e656fbe
Biggbie <pasha777levda@yandex.ru>
2024-03-26 17:51:12 +0300

    Фикс мелких ошибок

--- src/main/java/ru/plevda/vacation/salary/calculator/model/Vacation.java
+++ src/main/java/ru/plevda/vacation/salary/calculator/model/Vacation.java
@@ -2,11 +2,12 @@ package ru.plevda.vacation.salary.calculator.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class Vacation {
     private double averageSalary;
     private int vacationDays;
-    private List<Integer> vacationDates;
+    private List<LocalDate> vacationDates;
 }
