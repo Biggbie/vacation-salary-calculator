476d593a1428d559f9158d99d443f44801d1d52d
Biggbie <pasha777levda@yandex.ru>
2024-03-27 17:03:58 +0300

    Изменил тесты

--- src/test/java/ru/plevda/vacation/salary/calculator/service/VacationCalculatorServiceTest.java
+++ src/test/java/ru/plevda/vacation/salary/calculator/service/VacationCalculatorServiceTest.java
@@ -1,10 +1,20 @@
 package ru.plevda.vacation.salary.calculator.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VacationCalculatorServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
     @Test
     void calculate() {
